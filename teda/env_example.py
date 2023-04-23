e25f9da9ef790c92d80a615e798c27c242a38562
daniil7 <daniil.k@mail.ru>
2023-04-24 02:17:19 +0500

    first commit

---
+++ teda/env_example.py
@@ -0,0 +1,7 @@
+
+# Database settings
+
+# Используемая база данных MySQL/MariaDB
+database_name = "Название БД, например teda"
+database_password = "Твой пароль от БД"
+database_user = "Твой логин для БД"
