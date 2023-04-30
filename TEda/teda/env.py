ef155ea47f519bad466d294d938bc4dc65b46a84
daniil7 <daniil.k@mail.ru>
2023-04-30 19:44:23 +0500

    fix branch

--- TEda/teda/env.py
+++
@@ -1,8 +0,0 @@
-
-debug = True
-
-# Настройки базы данных. Не используются при env_debug = True
-# Используемая база данных MySQL/MariaDB
-database_name = "Название БД, например teda"
-database_password = "Твой пароль от БД"
-database_user = "Твой логин для БД"
