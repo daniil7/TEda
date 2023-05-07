6572bcbdd7db590e4423985462294efd7c5e7575
daniil7 <33127116+daniil7@users.noreply.github.com>
2023-05-07 11:55:22 +0500

    Thumbnails (#2)
    * created autogenerative thumbnails for dish images
    * show thumbnails in django admin panel

---
+++ catalog/services/create_thumbnail.py
@@ -0,0 +1,25 @@
+
+from django.conf import settings
+from PIL import Image
+import os
+
+def createThumbnail(image_path):
+    try:
+        image = Image.open(image_path)
+    except:
+        return False
+    image.thumbnail((200,200), Image.ANTIALIAS)
+    image_filename = os.path.basename(image_path)
+    thumb_name, thumb_extension = os.path.splitext(image_filename)
+    thumb_extension = thumb_extension.lower()
+
+    if thumb_extension in ['.jpg', '.jpeg']:
+        FTYPE = 'JPEG'
+    elif thumb_extension == '.gif':
+        FTYPE = 'GIF'
+    elif thumb_extension == '.png':
+        FTYPE = 'PNG'
+    else:
+        return False    # Unrecognized file type
+
+    return image
