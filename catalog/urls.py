b47f2a7a08adc31b761bf6a6319f08be7a9674e0
daniil7 <daniil.k@mail.ru>
2023-06-06 13:58:57 +0500

    DRF api initial commmit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import *
 from catalog import views
+from django.urls import include
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,13 +9,19 @@ urlpatterns = [
     path('category/<int:category_id>/', views.index_category, name='index_category'),
 
     # cart
+    #
     path('cart/plus/', views.PlusToCart.as_view(), name='cart_plus'),
     path('cart/minus/', views.MinusToCart.as_view(), name='cart_minus'),
     path('cart/remove/', views.RemoveFromCart.as_view(), name='cart_remove'),
 
+    # account
+    #
     path('account/', include('django.contrib.auth.urls')),
     path('account/register/', views.RegisterUser.as_view(), name='register'),
-
     #path('login/', views.LoginUser),
+
+    # api
+    #
+    path('api/', include('catalog.urls_api')),
 
 ]
